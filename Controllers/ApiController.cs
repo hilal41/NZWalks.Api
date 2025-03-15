634f5266292df835d297f1de9f09c03c0e28c6af
Hilal PC <Hilal PC@DESKTOP-LPSLHNS>
2025-03-15 13:10:45 +0500

    Add a new Controller and also GetAllRegions Using HttpGet

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.Api.Data;
+using NZWalks.Api.Models.Domain;
+
+namespace NZWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly WalksDbContext _dbContext;
+
+        public ApiController(WalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = _dbContext.regions.ToList();
+            return Ok(regions); 
+        }
+
+         
+        
+        
+
+
+    }
+}
