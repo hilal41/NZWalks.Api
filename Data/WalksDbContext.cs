8fca7b2bab7913ec8d17f9f95b1c22350d52ab04
Hilal PC <Hilal PC@DESKTOP-LPSLHNS>
2025-03-15 12:23:03 +0500

    create dbcontext class

---
+++ Data/WalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Api.Models.Domain;
+
+namespace NZWalks.Api.Data
+{
+    public class WalksDbContext : DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Difficulty> difficulties { get; set; }
+        public DbSet<Region> regions { get; set; }
+        public DbSet<Walk> walks { get; set; }
+    }
+    
+    }
+
