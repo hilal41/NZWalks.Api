c02f820c42edc8226aad3ca65eccc740024b7536
Hilal PC <Hilal PC@DESKTOP-LPSLHNS>
2025-04-13 00:20:05 +0500

    seed some data in the region table and also in the defficulty table

--- Data/WalksDbContext.cs
+++ Data/WalksDbContext.cs
@@ -12,7 +12,66 @@ namespace NZWalks.Api.Data
         public DbSet<Difficulty> difficulties { get; set; }
         public DbSet<Region> regions { get; set; }
         public DbSet<Walk> walks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed Data
+            var difficulties = new List<Difficulty>()
+                {
+                    new Difficulty()
+                    {
+                        Id = Guid.Parse("e3cb226c-f92b-4699-aed3-5f688ed43baf"),
+                        Name = "Easy"
+                    },
+                    new Difficulty()
+                    {
+                        Id = Guid.Parse("a4eb412c-cd0b-4fae-93de-5280b7f98cba"),
+                        Name = "Medium"
+                    },
+                    new Difficulty()
+                    {
+                        Id = Guid.Parse("5cf21b0c-4d90-480b-8ace-dc6da688abd1"),
+                        Name = "Hard"
+
+
+                       
+                    }
+                };
+
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+            var regions = new List<Region>()
+            {
+                new Region()
+                {
+                    Id = Guid.Parse("e3cb226c-f92b-4699-aed3-5f688ed43baf"),
+                    Code = "NI",
+                    Name = "North Island",
+                    RegionImageUrl = "https://example.com/region1.jpg"
+                },
+                new Region()
+                {
+                    Id = Guid.Parse("a4eb412c-cd0b-4fae-93de-5280b7f98cba"),
+                    Code = "SI",
+                    Name = "South Island",
+                    RegionImageUrl = "https://example.com/region2.jpg"
+                },
+
+                new Region()
+                {
+                    Id = Guid.Parse("5cf21b0c-4d90-480b-8ace-dc6da688abd1"),
+                    Code = "OT",
+                    Name = "Other",
+                    RegionImageUrl = "https://example.com/region3.jpg"
+                },
+            };
+
+            modelBuilder.Entity<Region>().HasData(regions);
+
+
+        }
     }
-    
     }
+
 
---
+++ Migrations/20250412191116_ def Create.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class defCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("5cf21b0c-4d90-480b-8ace-dc6da688abd1"), "Hard" },
+                    { new Guid("a4eb412c-cd0b-4fae-93de-5280b7f98cba"), "Medium" },
+                    { new Guid("e3cb226c-f92b-4699-aed3-5f688ed43baf"), "Easy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("5cf21b0c-4d90-480b-8ace-dc6da688abd1"), "OT", "Other", "https://example.com/region3.jpg" },
+                    { new Guid("a4eb412c-cd0b-4fae-93de-5280b7f98cba"), "SI", "South Island", "https://example.com/region2.jpg" },
+                    { new Guid("e3cb226c-f92b-4699-aed3-5f688ed43baf"), "NI", "North Island", "https://example.com/region1.jpg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("5cf21b0c-4d90-480b-8ace-dc6da688abd1"));
+
+            migrationBuilder.DeleteData(
+                table: "difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("a4eb412c-cd0b-4fae-93de-5280b7f98cba"));
+
+            migrationBuilder.DeleteData(
+                table: "difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("e3cb226c-f92b-4699-aed3-5f688ed43baf"));
+
+            migrationBuilder.DeleteData(
+                table: "regions",
+                keyColumn: "Id",
+                keyValue: new Guid("5cf21b0c-4d90-480b-8ace-dc6da688abd1"));
+
+            migrationBuilder.DeleteData(
+                table: "regions",
+                keyColumn: "Id",
+                keyValue: new Guid("a4eb412c-cd0b-4fae-93de-5280b7f98cba"));
+
+            migrationBuilder.DeleteData(
+                table: "regions",
+                keyColumn: "Id",
+                keyValue: new Guid("e3cb226c-f92b-4699-aed3-5f688ed43baf"));
+        }
+    }
+}
